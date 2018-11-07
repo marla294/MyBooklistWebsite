1dd151544d19f418a1b46a56866ee8d898db8415
Marla Foreman <marla294@gmail.com>
2018-11-07 15:40:28 -0600

    Creating Book component

---
+++ src/Components/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+class Book extends React.Component {
+	render() {
+		return <pre>Book Component</pre>;
+	}
+}
+
+// Book.propTypes = {};
+
+export default Book;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Book from "./Book";
 
 class Home extends React.Component {
 	componentDidMount() {
@@ -10,7 +11,7 @@ class Home extends React.Component {
 	render() {
 		return (
 			<div className="home">
-				<p>Hi</p>
+				<Book />
 			</div>
 		);
 	}
