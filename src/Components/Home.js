0cd018775e19ba0bb3e054221840d1d331d980b4
Marla Foreman <marla294@gmail.com>
2018-11-07 15:32:39 -0600

    Fetching booklist data in Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 class Home extends React.Component {
+	componentDidMount() {
+		fetch("http://127.0.0.1:8080/api/BookList").then(res => {
+			res.json().then(r => console.log(r));
+		});
+	}
+
 	render() {
 		return (
 			<div className="home">
