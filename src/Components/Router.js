2bc2466040ac1780e1b99ecf1d7f78b37162d07a
Marla Foreman <marla294@gmail.com>
2018-11-07 11:40:08 -0600

    Creating Router component

---
+++ src/Components/Router.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+
+const Router = props => (
+	<BrowserRouter>
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route exact path="/Home" component={Home} />
+		</Switch>
+	</BrowserRouter>
+);
+
+export default Router;
