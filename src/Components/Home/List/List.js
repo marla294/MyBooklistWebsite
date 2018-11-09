d6b7870665a5051e295546c82d6803ca3f095c76
Marla Foreman <marla294@gmail.com>
2018-11-09 11:05:44 -0600

    Making List into a functional component

--- src/Components/Home/List/List.js
+++ src/Components/Home/List/List.js
@@ -2,21 +2,19 @@ import React from "react";
 import Book from "./Book/Book";
 import PropTypes from "prop-types";
 
-class List extends React.Component {
-	renderBooks = () => {
-		let bookList = this.props.bookList;
-		if (bookList.length === 0) {
-			return "Loading...";
-		} else {
-			return bookList.map(book => {
-				return <Book key={book.Id} book={book} />;
-			});
-		}
-	};
-
-	render() {
-		return <pre className="list">{this.renderBooks()}</pre>;
+function renderBooks(props) {
+	let bookList = props.bookList;
+	if (bookList.length === 0) {
+		return "Loading...";
+	} else {
+		return bookList.map(book => {
+			return <Book key={book.Id} book={book} />;
+		});
 	}
+}
+
+function List(props) {
+	return <pre className="list">{renderBooks(props)}</pre>;
 }
 
 List.propTypes = {
