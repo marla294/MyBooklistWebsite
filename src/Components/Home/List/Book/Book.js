393142c4fdfc0a725ce0841ebcc8da72e0901b90
Marla Foreman <marla294@gmail.com>
2018-11-09 11:09:55 -0600

    Making Book a functional component

--- src/Components/Home/List/Book/Book.js
+++ src/Components/Home/List/Book/Book.js
@@ -1,24 +1,20 @@
 import React from "react";
 import { capitalizeFirstLetterEachWord } from "../../../../HelperFunctions";
-
 import PropTypes from "prop-types";
 
-class Book extends React.Component {
-	renderBook = () => {
-		let book = this.props.book.Book;
-		let title = capitalizeFirstLetterEachWord(book.Name) || "";
-		let url = book.URL || "";
+function renderBook(props) {
+	let book = props.book.Book;
+	let title = capitalizeFirstLetterEachWord(book.Name) || "";
 
-		return (
-			<div className="book">
-				<div className="title">{title}</div>
-			</div>
-		);
-	};
+	return (
+		<div className="book">
+			<div className="title">{title}</div>
+		</div>
+	);
+}
 
-	render() {
-		return <pre>{this.renderBook()}</pre>;
-	}
+function Book(props) {
+	return <pre>{renderBook(props)}</pre>;
 }
 
 Book.propTypes = {
