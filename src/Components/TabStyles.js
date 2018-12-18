0e208991aecad60ecdb3bb1fbbb5cf89cce0abfc
Marla Foreman <marla294@gmail.com>
2018-12-18 16:04:18 -0600

    Finished styling the Tab component

--- src/Components/TabStyles.js
+++ src/Components/TabStyles.js
@@ -1,45 +1,5 @@
 import styled from "styled-components";
 
-// export const TabStyles = styled.div`
-// 	grid-column: ${props => props.count};
-// 	display: grid;
-// 	grid-template-columns: ${props =>
-// 		props.type === "NewList" ? "1fr" : "15fr 1fr"};
-// 	border-bottom: ${props => props.theme.S07} solid ${props => {
-// 	return props.selected ? props.theme.blue : props.theme.darkblue;
-// }}
-// 	border-right: ${props => props.theme.S04} solid rgba(255, 0, 0, 0);
-// 	border-left: ${props => props.theme.S04} solid rgba(255, 0, 0, 0);
-// 	font-size: ${props => props.theme.F06};
-// 	width: ${props => props.theme.S12};
-// 	height: 0em;
-// 	padding-left: ${props => props.theme.S01};
-// 	z-index: ${props => (props.selected ? 10 : 2)};
-
-// 	input {
-// 		color: ${props => (props.selected ? props.theme.black : props.theme.gray)};
-// 		background: ${props => {
-// 			return props.selected ? props.theme.blue : props.theme.darkblue;
-// 		}}
-// 		font-size: ${props => props.theme.F06};
-// 		width: 95%;
-// 		text-align: left;
-// 		border: none;
-// 		outline: none;
-// 		cursor: pointer;
-// 	}
-
-// 	button {
-// 		color: ${props => (props.selected ? props.theme.black : props.theme.gray)};
-// 		background: ${props =>
-// 			props.selected ? props.theme.blue : props.theme.darkblue};
-// 		font-size: ${props => props.theme.F08};
-// 		border: none;
-// 		outline: none;
-// 	}
-
-// `;
-
 export const TabStyles = styled.div`
 	width: ${props => props.theme.S11};
 	height: 0;
@@ -53,24 +13,27 @@ export const TabStyles = styled.div`
 	grid-template-columns: 15fr 1fr;
 
 	* {
+		height: ${props => props.theme.S06};
+
 		color: ${props => props.theme.black};
-		background: ${props => props.theme.yellow};
+		background: ${props => props.theme.blue};
+
 		border: none;
 		outline: none;
 		cursor: pointer;
-		height: ${props => props.theme.S06};
-		display: grid;
 	}
 
 	input {
 		width: 95%;
+
 		font-size: ${props => props.theme.F03};
 	}
 
 	button {
+		padding: 0 5px 10px 0;
+
 		font-size: ${props => props.theme.F07};
-		display: inline-block;
-		line-height: ${props => props.theme.S06};
-		vertical-align: center;
+		text-align: center;
+		line-height: ${props => props.theme.S05};
 	}
 `;
