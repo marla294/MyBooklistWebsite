434fd90d9e30c7d3068f4f95cc3eba9a4ac687d4
Marla Foreman <marla294@gmail.com>
2018-12-20 17:01:57 -0600

    Converted Tab to be a component class so that I can set a ref on the input to force it to blur on enter
    Grrr…

--- src/Components/Tab.js
+++ src/Components/Tab.js
@@ -1,13 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-
-Tab.propTypes = {
-	id: PropTypes.number.isRequired,
-	listTitle: PropTypes.string.isRequired,
-	updateListTitle: PropTypes.func.isRequired,
-	deleteList: PropTypes.func.isRequired
-};
 
 const TabStyles = styled.div`
 	width: 100%;
@@ -40,23 +33,42 @@ const TabStyles = styled.div`
 /* 
 The Tab component is the title of the list being shown on the screen.  It is meant to look like a file tab.  You can edit the list name by typing in the new name and clicking enter or clicking off the tab.  You can delete the list name by clicking the "x" button on the side of the tab.  An other list that you have created will show up in the deleted list's place.
 */
-export default function Tab(props) {
-	const [listTitle, setListTitle] = useState(props.listTitle);
+export default class Tab extends React.Component {
+	state = {
+		listTitle: this.props.listTitle
+	};
 
-	return (
-		<TabStyles>
-			<input
-				value={listTitle}
-				onChange={event => setListTitle(event.target.value)}
-				onKeyPress={async event => {
-					if (event.key === "Enter") {
-						await props.updateListTitle(props.id, listTitle);
+	render() {
+		return (
+			<TabStyles>
+				<input
+					value={this.state.listTitle}
+					onChange={event =>
+						this.setState({ listTitle: event.target.value })
 					}
-				}}
-				onBlur={async event => {
-					await props.updateListTitle(props.id, listTitle);
-				}}
-			/>
-		</TabStyles>
-	);
+					onKeyPress={async event => {
+						if (event.key === "Enter") {
+							await this.props.updateListTitle(
+								this.props.id,
+								this.state.listTitle
+							);
+						}
+					}}
+					onBlur={async event => {
+						await this.props.updateListTitle(
+							this.props.id,
+							this.state.listTitle
+						);
+					}}
+				/>
+			</TabStyles>
+		);
+	}
 }
+
+Tab.propTypes = {
+	id: PropTypes.number.isRequired,
+	listTitle: PropTypes.string.isRequired,
+	updateListTitle: PropTypes.func.isRequired,
+	deleteList: PropTypes.func.isRequired
+};
