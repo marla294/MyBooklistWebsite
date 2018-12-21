543314892727426a9645c7e33136478a5d288a95
Marla Foreman <marla294@gmail.com>
2018-12-21 12:47:10 -0600

    Continuing to style dropdown - almost there

--- src/Components/TabDropdown.js
+++ src/Components/TabDropdown.js
@@ -18,47 +18,22 @@ const TabDropButton = styled.button`
 	color: ${props => props.theme.gray};
 
 	:hover {
-		color: ${props => props.theme.blue};
+		color: ${props => props.theme.yellow};
 	}
 `;
 
 const Dropdown = styled.div`
 	position: absolute;
 	right: 8px;
-	border: 1px solid black;
+	border: 0.3rem solid ${props => props.theme.yellow};
 	background: white;
 	display: ${props => (props.showDropdown ? "grid" : "none")};
 	z-index: 1000;
+	box-shadow: ${props => props.theme.bs};
 `;
 
-// const Option = styled.div`
-// 	max-width: 200px;
-// 	padding: ${props => props.theme.S03};
-// 	overflow: scroll;
-// 	color: ${props => props.theme.black};
-// 	font-size: ${props => props.theme.F01};
-// 	cursor: pointer;
-
-// 	button {
-// 		padding: 0 5px 5px 0;
-
-// 		background: white;
-// 		color: ${props => props.theme.black};
-
-// 		font-size: ${props => props.theme.F07};
-// 		text-align: center;
-// 		line-height: ${props => props.theme.S05};
-
-// 		outline: none;
-// 	}
-
-// 	:hover {
-// 		background-color: ${props => props.theme.gray};
-// 	}
-// `;
-
 const Option = styled.a`
-	max-width: ${props => props.theme.S11};
+	max-width: ${props => props.theme.S12};
 	padding: ${props => props.theme.S03};
 	color: ${props => props.theme.black};
 
@@ -67,17 +42,19 @@ const Option = styled.a`
 	align-items: center;
 	cursor: pointer;
 
-	button {
-		height: 40px;
-		font-size: ${props => props.theme.F07};
-		background-color: white;
-		padding-bottom: 8px;
-		margin-left: ${props => props.theme.S02};
-		border: none;
+	div {
+		height: 25px;
+		border-bottom: ${props => props.selected ? `0.3rem solid ${props.theme.yellow}` : "none"};
 	}
 
-	button:hover {
-		color: ${props => props.theme.red};
+	button {
+		height: 30px;
+		line-height: 0;
+		font-size: ${props => props.theme.F07};
+		background-color: white;
+		margin-left: ${props => props.theme.S02};
+		padding-bottom: 10px;
+		border: none;
 	}
 
 	:hover {
@@ -105,8 +82,8 @@ export default function TabDropdown(props) {
 	function renderDropdownOptions() {
 		const options = props.lists.map(list => {
 			return (
-				<Option key={list.Id} onClick={() => clickAnOption(list.Id)}>
-					{list.Name}
+				<Option key={list.Id} onClick={() => clickAnOption(list.Id)} selected={list.Id === props.selectedList}>
+					<div>{list.Name}</div>
 					<button
 						onClick={async () => {
 							await props.deleteList(list.Id);
