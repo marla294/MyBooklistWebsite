ae0994b0169c544048e0c4fa8619429b0ff308f4
Marla Foreman <marla294@gmail.com>
2018-12-22 12:32:28 -0600

    Styling NewBook the same as the Book component

--- src/Components/NewBook.js
+++ src/Components/NewBook.js
@@ -1,12 +1,71 @@
-import React from "react";
-// import PropTypes from "prop-types";
-import { useState } from "react";
-import {
-	NewBookWrapper,
-	BookWrapper,
-	BookCoverImage,
-	TitleAuthorWrapper
-} from "./NewBookStyles";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+// export const NewBookWrapper = styled.div`
+// 	background: ${props => props.theme.yellow};
+// 	display: ${props => (props.displayNewBook ? "grid" : "none")};
+// 	grid-template-rows: auto auto;
+// 	grid-gap: ${props => props.theme.S04};
+// 	width: 100%;
+// 	padding: ${props => props.theme.S04};
+// 	h1 {
+// 		justify-self: center;
+// 	}
+// `;
+
+// export const BookWrapper = styled.div`
+// 	display: grid;
+// 	grid-template-columns: 6rem auto;
+// 	width: 100%;
+// 	grid-gap: ${props => props.theme.S04};
+// `;
+
+// export const BookCoverImage = styled.img`
+// 	justify-self: center;
+// 	width: 5rem;
+// `;
+
+// export const TitleAuthorWrapper = styled.fieldset`
+// 	align-self: center;
+// 	border: none;
+// 	display: grid;
+// 	grid-template-rows: repeat(3, auto);
+// 	input {
+// 		color: ${props => props.theme.black};
+// 		background: ${props => props.theme.yellow};
+// 		border: none;
+// 		outline: none;
+// 		font-size: 2rem;
+// 	}
+// 	label {
+// 		font-size: 2rem;
+// 	}
+// `;
+
+export const BookWrapper = styled.div`
+	background: ${props => props.theme.yellow};
+	padding: ${props => props.theme.S05};
+	border: 0.3rem solid ${props => props.theme.orange};
+	display: ${props => (props.displayNewBook ? "grid" : "none")};
+`;
+
+export const TitleAuthorWrapper = styled.div`
+	align-self: center;
+	display: grid;
+	grid-gap: ${props => props.theme.S02};
+`;
+
+export const Title = styled.h1`
+	font-size: ${props => props.theme.F04};
+	color: ${props => props.theme.darkorange};
+	font-weight: 900;
+`;
+
+export const Author = styled.h3`
+	font-size: ${props => props.theme.F03};
+	color: ${props => props.theme.orange};
+	font-weight: 900;
+`;
 
 export default function NewBook(props) {
 	const [title, setTitle] = useState("New Title");
@@ -20,50 +79,36 @@ export default function NewBook(props) {
 	};
 
 	return (
-		<NewBookWrapper displayNewBook={props.displayNewBook}>
-			<h1>Add a new book!</h1>
-			<BookWrapper>
-				<BookCoverImage
-					src={
-						"https://www.freeclipartnow.com/d/11081-1/open-book-01.jpg"
-					}
-				/>
-				<form
-					onSubmit={async e => {
-						e.preventDefault();
-						const bookId = await props.addBook(title, author);
-						await props.addBookToList(bookId, props.listId);
-					}}
-				>
-					<TitleAuthorWrapper>
-						<label htmlFor="title">
-							Title:
-							<input
-								type="text"
-								id="title"
-								name="title"
-								placeholder="Title"
-								required
-								value={title}
-								onChange={handleChange}
-							/>
-						</label>
-						<label htmlFor="author">
-							Author:
-							<input
-								type="text"
-								id="author"
-								name="author"
-								placeholder="Author"
-								required
-								value={author}
-								onChange={handleChange}
-							/>
-						</label>
-						<button type="submit">Submit</button>
-					</TitleAuthorWrapper>
-				</form>
-			</BookWrapper>
-		</NewBookWrapper>
+		<BookWrapper displayNewBook={props.displayNewBook}>
+			<form
+				onSubmit={async e => {
+					e.preventDefault();
+					const bookId = await props.addBook(title, author);
+					await props.addBookToList(bookId, props.listId);
+				}}
+			>
+				<TitleAuthorWrapper>
+					<input
+						type="text"
+						id="title"
+						name="title"
+						placeholder="Title"
+						required
+						value={title}
+						onChange={handleChange}
+					/>
+					<input
+						type="text"
+						id="author"
+						name="author"
+						placeholder="Author"
+						required
+						value={author}
+						onChange={handleChange}
+					/>
+					<button type="submit">Submit</button>
+				</TitleAuthorWrapper>
+			</form>
+		</BookWrapper>
 	);
 }
