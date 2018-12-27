9a5f9426d68960c9d69eb31690215993f2d9b222
Marla Foreman <marla294@gmail.com>
2018-12-27 17:09:35 -0600

    Getting bare bones forms set up for SignIn and SignUp components

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const SignInWrapper = styled.div`
@@ -18,9 +18,38 @@ const SignInWrapper = styled.div`
 `;
 
 export default function SignIn(props) {
+	const [username, setUsername] = useState("Username");
+	const [password, setPassword] = useState("");
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+
+		if (name === "username") setUsername(value);
+		if (name === "password") setPassword(value);
+	};
+
 	return (
 		<SignInWrapper>
 			<p>Sign in component</p>
+			<form>
+				<input
+					type="text"
+					id="username"
+					name="username"
+					required
+					value={username}
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					required
+					value={password}
+					onChange={handleChange}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 		</SignInWrapper>
 	);
 }
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const SignUpWrapper = styled.div`
@@ -18,9 +18,48 @@ const SignUpWrapper = styled.div`
 `;
 
 export default function SignUp(props) {
+	const [username, setUsername] = useState("Username");
+	const [password, setPassword] = useState("");
+	const [confirm, setConfirm] = useState("");
+
+	const handleChange = e => {
+		const { name, value } = e.target;
+
+		if (name === "username") setUsername(value);
+		if (name === "password") setPassword(value);
+		if (name === "confirm") setConfirm(value);
+	};
+
 	return (
 		<SignUpWrapper>
 			<p>Sign up component</p>
+			<form>
+				<input
+					type="text"
+					id="username"
+					name="username"
+					required
+					value={username}
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					id="password"
+					name="password"
+					required
+					value={password}
+					onChange={handleChange}
+				/>
+				<input
+					type="password"
+					id="confirm"
+					name="confirm"
+					required
+					value={confirm}
+					onChange={handleChange}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 		</SignUpWrapper>
 	);
 }
