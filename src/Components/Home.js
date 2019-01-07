ec1cafef6bb02b840c0b6897a81a3abc298b1d3d
Marla Foreman <marla294@gmail.com>
2019-01-06 18:14:03 -0600

    Getting rid of headers on fetch

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -309,7 +309,6 @@ export default function Home(props) {
 	const fetchPostUser = async (name, username, password) => {
 		const result = await fetch(url + "Users", {
 			method: "POST",
-			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
 				name,
 				username: username.toLowerCase(),
