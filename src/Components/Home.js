962a7ee488bdacffbc21edac62c2e5c3fd4f4506
Marla Foreman <marla294@gmail.com>
2019-01-10 11:06:19 -0600

    Forgot to set the url back to prod

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -49,8 +49,8 @@ const Loading = styled.h2`
 `;
 
 export default function Home(props) {
-	const url = "http://127.0.0.1:8080/api/";
-	// const url = "https://www.axequest.com/booklist/api/";
+	// const url = "http://127.0.0.1:8080/api/";
+	const url = "https://www.axequest.com/booklist/api/";
 	const [bookList, setBookList] = useState(null);
 	const [lists, setLists] = useState(null);
 	const [selectedList, setSelected] = useState(null);
